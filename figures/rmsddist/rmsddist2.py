0894941bfd93a4e92fb05358cb38da92423f3b5b
Daniel L. Parton <Danny.Parton@gmail.com>
2015-02-18 19:09:16 -0500

    Added rmsddist figure v2

---
+++ figures/rmsddist/rmsddist2.py
@@ -0,0 +1,68 @@
+import os
+import ensembler
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data_dir = os.path.join('..', '..', 'data')
+targets = open(os.path.join(data_dir, 'TKs.txt')).read().splitlines()
+
+models_target_dir = os.path.join(data_dir, 'models', targets[0])
+rmsds_filepath = os.path.join(models_target_dir, 'rmsds.csv')
+combined_dfs = pd.read_csv(rmsds_filepath, index_col=0)
+
+for target in targets[1:]:
+    models_target_dir = os.path.join(data_dir, 'models', target)
+    rmsds_filepath = os.path.join(models_target_dir, 'rmsds.csv')
+    combined_dfs = pd.concat([combined_dfs, pd.read_csv(rmsds_filepath, index_col=0)]).reset_index(drop=True)
+
+df_has_model = combined_dfs[combined_dfs.has_model]
+
+seqid_mins = np.arange(0., 100., 100./3.)
+seqid_maxs = np.arange(100./3., 101., 100./3.)
+seqid_ranges = zip(seqid_mins, seqid_maxs)
+
+seqid_maxs[-1] = 101.
+
+sns.set(style="white")
+
+# palette = sns.blend_palette([sns.desaturate('royalblue', 0), 'royalblue'], 5)
+palette = sns.color_palette('GnBu_d', n_colors=3)[::-1]
+
+# sns.kdeplot(df_has_model.rmsd*10, label='0-100 ($n=$%d)' % len(df_has_model), color='red', bw=02.5)
+
+for i, seqid_range in enumerate(seqid_ranges):
+    df_seqid_bin = df_has_model[df_has_model.seqid >= seqid_range[0]][df_has_model.seqid < seqid_range[1]]
+    print seqid_range, len(df_seqid_bin)
+    label = '%.0f-%.0f ($n=$%d)' % (seqid_range[0], seqid_range[1], len(df_seqid_bin))
+
+    # sns.distplot(df_seqid_bin.rmsd, label=labels[i], hist=False)
+    sns.kdeplot(df_seqid_bin.rmsd*10, label=label, color=palette[i], bw=2.5)
+
+    # if i == 4:
+    #     sns.rugplot(df_seqid_bin.rmsd*10, color=palette[i])
+
+    # hgram = histogram(df_seqid_bin.rmsd, bins=np.arange(0.,6.,0.5), density=True)
+    # x = hgram[1][:-1] + ((hgram[1][1] - hgram[1][0])/2.)
+    # plot(x, hgram[0], label=seqid_range[0], color=colors[i])
+
+ax = plt.gca()
+plt.setp(ax, yticks=[])
+
+plt.legend(title='Sequence identity (%)')
+plt.xlim(0,40)
+plt.xlabel('RMSD ($\AA$)')
+sns.despine(left=True)
+plt.savefig('rmsddist2.png')
+
+plt.close()
+
+# Plot sequence identity distribution
+
+# sns.set(style="white")
+# sns.kdeplot(df_has_model.seqid)
+# sns.despine(left=True)
+# plt.setp(plt.gca(), yticks=[])
+# plt.xlim(0,102)
+# plt.savefig('seqid_dist.png')
