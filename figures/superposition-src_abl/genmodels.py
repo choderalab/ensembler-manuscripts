9c4501a6a147ac23c32c57015bced13a60d94d74
Daniel L. Parton <Danny.Parton@gmail.com>
2015-02-24 22:32:30 -0500

    Added superposition figure

---
+++ figures/superposition-src_abl/genmodels.py
@@ -0,0 +1,77 @@
+import os
+import shutil
+import tempfile
+import numpy as np
+import pandas as pd
+import mdtraj
+import seaborn as sns
+
+srcdir = '../../data/models/SRC_HUMAN_D0'
+
+df = pd.read_csv(os.path.join(srcdir, 'modelstraj-data.csv'))
+
+traj = mdtraj.load(os.path.join(srcdir, 'modelstraj.xtc'), top=os.path.join(srcdir, 'modelstraj-topol.pdb'))
+
+nmodels = 20
+
+modelsdir = 'models'
+if os.path.exists(modelsdir):
+    shutil.rmtree(modelsdir)
+    os.mkdir(modelsdir)
+else:
+    os.mkdir(modelsdir)
+
+# model_indices = np.arange(nmodels) * (len(traj) / nmodels)
+
+desired_seqid_values = 100. - np.arange(0., 100., 100./nmodels)
+model_indices = [np.abs(df.seqid.values-seqid_value).argmin() for seqid_value in desired_seqid_values]
+model_indices = sorted(list(set(model_indices)))
+nmodels = len(model_indices)
+seqids = [df.seqid.values[m] for m in model_indices]
+
+print 'Number of models selected:', nmodels
+print model_indices
+print seqids
+
+for m,f in enumerate(model_indices):
+    frame = traj[f]
+    frame.save_pdb(os.path.join(modelsdir, '{0}.pdb'.format(m)))
+
+
+# seqids = [df.seqid[f] for f in model_indices]
+transparency_floats = [(1. - seqid*0.01) for seqid in seqids]
+transparency_text = '\n'.join(['set cartoon_transparency, {0}, {1}'.format(transparency_floats[m], m) for m in range(nmodels)])
+
+palette = sns.color_palette('deep', nmodels)
+set_color_text = '\n'.join(['set_color {0}= [{1}, {2}, {3}]'.format(m, palette[m][0], palette[m][1], palette[m][2]) for m in range(nmodels)])
+color_text = '\n'.join(['color {0}, {0}'.format(m) for m in range(nmodels)])
+
+renderscripttext = """\
+bg white
+# set ray_opaque_background, off
+# set ray_trace_mode, 0
+# set ambient, 1
+# set reflect, 0
+set antialias, 1
+set two_sided_lighting, on
+set cartoon_fancy_helices, 1
+
+hide
+show cartoon
+
+{0}
+
+{1}
+
+{2}
+
+color black
+
+zoom all
+
+# ray 1280,960
+# png superposed.png
+""".format(transparency_text, set_color_text, color_text)
+
+with open('render.pml', 'w') as renderscriptfile:
+    renderscriptfile.write(renderscripttext)
---
+++ figures/superposition-src_abl/render.pml
@@ -0,0 +1,63 @@
+bg white
+# set ray_opaque_background, off
+# set ray_trace_mode, 0
+# set ambient, 1
+# set reflect, 0
+set antialias, 1
+set two_sided_lighting, on
+set cartoon_fancy_helices, 1
+
+hide
+show cartoon
+
+set cartoon_transparency, 0.0, 0
+set cartoon_transparency, 0.03, 1
+set cartoon_transparency, 0.313, 2
+set cartoon_transparency, 0.352, 3
+set cartoon_transparency, 0.356, 4
+set cartoon_transparency, 0.484, 5
+set cartoon_transparency, 0.5, 6
+set cartoon_transparency, 0.551, 7
+set cartoon_transparency, 0.599, 8
+set cartoon_transparency, 0.65, 9
+set cartoon_transparency, 0.7, 10
+set cartoon_transparency, 0.75, 11
+set cartoon_transparency, 0.799, 12
+set cartoon_transparency, 0.837, 13
+
+set_color 0= [0.298039215686, 0.447058823529, 0.690196078431]
+set_color 1= [0.333333333333, 0.658823529412, 0.407843137255]
+set_color 2= [0.76862745098, 0.305882352941, 0.321568627451]
+set_color 3= [0.505882352941, 0.447058823529, 0.698039215686]
+set_color 4= [0.8, 0.725490196078, 0.454901960784]
+set_color 5= [0.392156862745, 0.709803921569, 0.803921568627]
+set_color 6= [0.298039215686, 0.447058823529, 0.690196078431]
+set_color 7= [0.333333333333, 0.658823529412, 0.407843137255]
+set_color 8= [0.76862745098, 0.305882352941, 0.321568627451]
+set_color 9= [0.505882352941, 0.447058823529, 0.698039215686]
+set_color 10= [0.8, 0.725490196078, 0.454901960784]
+set_color 11= [0.392156862745, 0.709803921569, 0.803921568627]
+set_color 12= [0.298039215686, 0.447058823529, 0.690196078431]
+set_color 13= [0.333333333333, 0.658823529412, 0.407843137255]
+
+color 0, 0
+color 1, 1
+color 2, 2
+color 3, 3
+color 4, 4
+color 5, 5
+color 6, 6
+color 7, 7
+color 8, 8
+color 9, 9
+color 10, 10
+color 11, 11
+color 12, 12
+color 13, 13
+
+color black
+
+zoom all
+
+# ray 1280,960
+# png superposed.png
